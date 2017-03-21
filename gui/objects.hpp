d4832b09eb0a844d7ad510c6ceed0f557e807c23
Sultan Qasim Khan <sultanqasim@gmail.com>
2017-03-20 21:41:23 -0400

    [SQUASH] Remove TWRP app
    Revert "Add TWRP app install to landscape and watch themes"
    
    This reverts commit 64c5c0a9ba01f590d15bd0969d92d1473daf6600.
    
    Revert "Fix crash during check for TWRP app"
    
    This reverts commit 5128d299a5eab93348a5ef2ffff0e38d599289bc.
    
    Revert "Fix detection of TWRP app in /data"
    
    This reverts commit 5e1a7f932c39b96508c6dd417cf449494d901204.
    
    Revert "Add TWRP app install via TWRP recovery"
    
    This reverts commit b4bff5e9d1f514063aaef9ffad870fe13ed70bfb.
    
    Change-Id: Ibbc14190d361d6e864f0ab38636b7abf5d30b0a2

--- gui/objects.hpp
+++ gui/objects.hpp
@@ -326,7 +326,6 @@ protected:
 	int getpartitiondetails(std::string arg);
 	int screenshot(std::string arg);
 	int setbrightness(std::string arg);
-	int checkforapp(std::string arg);
 
 	// (originally) threaded actions
 	int fileexists(std::string arg);
@@ -366,7 +365,6 @@ protected:
 	int togglebacklight(std::string arg);
 	int twcmd(std::string arg);
 	int setbootslot(std::string arg);
-	int installapp(std::string arg);
 
 	int simulate;
 };
